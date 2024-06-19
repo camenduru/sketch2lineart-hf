a50b44f42a8069167db2b5cae6c7a5af7872d653
tori29umai0123 <minami.ibon0123@gmail.com>
2024-06-19 17:59:50 +0900

    app.py

--- app.py
+++ app.py
@@ -72,94 +72,99 @@ pipe = pipe.to(device)
 
 
 
-@spaces.GPU
-def predict(
-    input_image_path,
-    prompt,
-    negative_prompt,
-    controlnet_conditioning_scale,
-):
-    input_image_pil = Image.open(input_image_path)
-    base_size =input_image_pil.size
-    resize_image= resize_image_aspect_ratio(input_image_pil)
-    resize_image_size = resize_image.size
-    width = resize_image_size[0]
-    height = resize_image_size[1]
-    white_base_pil = base_generation(resize_image.size, (255, 255, 255, 255)).convert("RGB")
-    conditioning, pooled = compel([prompt, negative_prompt])
-    generator = torch.manual_seed(0)
-    last_time = time.time()
+class Img2Img:
+    def __init__(self):
+        self.input_image_path = None
 
-    output_image = pipe(
-        image=white_base_pil,
-        control_image=resize_image,
-        strength=1.0,
-        prompt_embeds=conditioning[0:1],
-        pooled_prompt_embeds=pooled[0:1],
-        negative_prompt_embeds=conditioning[1:2],
-        negative_pooled_prompt_embeds=pooled[1:2],
-        width=width,
-        height=height,
-        controlnet_conditioning_scale=float(controlnet_conditioning_scale),
-        controlnet_start=0.0,
-        controlnet_end=1.0,
-        generator=generator,
-        num_inference_steps=30,
-        guidance_scale=8.5,
-        eta=1.0,
-    )
-    print(f"Time taken: {time.time() - last_time}")
-    output_image = output_image.resize(base_size, Image.LANCZOS)
-    return output_image
+    @spaces.GPU
+    def predict(
+        self,
+        input_image_path,
+        prompt,
+        negative_prompt,
+        controlnet_conditioning_scale,
+    ):
+        input_image_pil = Image.open(input_image_path)
+        base_size =input_image_pil.size
+        resize_image= resize_image_aspect_ratio(input_image_pil)
+        resize_image_size = resize_image.size
+        width = resize_image_size[0]
+        height = resize_image_size[1]
+        white_base_pil = base_generation(resize_image.size, (255, 255, 255, 255)).convert("RGB")
+        conditioning, pooled = compel([prompt, negative_prompt])
+        generator = torch.manual_seed(0)
+        last_time = time.time()
 
-
-css = """
-#intro{
-    # max-width: 32rem;
-    # text-align: center;
-    # margin: 0 auto;
-}
-"""
-
-with gr.Blocks(css=css) as demo:
-    with gr.Row() as block:
-        with gr.Column():
-            # 画像アップロード用の行
-            with gr.Row():
-                with gr.Column():
-                    input_image_path = gr.Image(label="入力画像",  type='filepath')
-            
-            # プロンプト入力用の行
-            with gr.Row():
-                prompt_analysis = PromptAnalysis(tagger_dir)
-                [prompt, nega] = prompt_analysis.layout(input_image_path)           
-            # 画像の詳細設定用のスライダー行
-            with gr.Row():
-                controlnet_conditioning_scale = gr.Slider(minimum=0.5, maximum=1.25, value=1.0, step=0.01, interactive=True, label="線画忠実度")
-           
-            # 画像生成ボタンの行
-            with gr.Row():
-                generate_button = gr.Button("生成", interactive=False)
-
-        with gr.Column():
-            output_image = gr.Image(type="pil", label="Output Image")
-
-        # インプットとアウトプットの設定
-        inputs = [
-            input_image_path,
-            prompt,
-            nega,
-            controlnet_conditioning_scale,
-        ]
-        outputs = [output_image]
-        
-        # ボタンのクリックイベントを設定
-        generate_button.click(
-            fn=predict,
-            inputs=[input_image_path, prompt, nega, controlnet_conditioning_scale],
-            outputs=[output_image]
+        output_image = pipe(
+            image=white_base_pil,
+            control_image=resize_image,
+            strength=1.0,
+            prompt_embeds=conditioning[0:1],
+            pooled_prompt_embeds=pooled[0:1],
+            negative_prompt_embeds=conditioning[1:2],
+            negative_pooled_prompt_embeds=pooled[1:2],
+            width=width,
+            height=height,
+            controlnet_conditioning_scale=float(controlnet_conditioning_scale),
+            controlnet_start=0.0,
+            controlnet_end=1.0,
+            generator=generator,
+            num_inference_steps=30,
+            guidance_scale=8.5,
+            eta=1.0,
         )
+        print(f"Time taken: {time.time() - last_time}")
+        output_image = output_image.resize(base_size, Image.LANCZOS)
+        return output_image
 
-# デモの設定と起動
-demo.queue(api_open=True)
-demo.launch(show_api=True)
+
+    css = """
+    #intro{
+        # max-width: 32rem;
+        # text-align: center;
+        # margin: 0 auto;
+    }
+    """
+    def layout(self):
+        with gr.Blocks(css=css) as demo:
+            with gr.Row() as block:
+                with gr.Column():
+                    # 画像アップロード用の行
+                    with gr.Row():
+                        with gr.Column():
+                            self.input_image_path = gr.Image(label="入力画像",  type='filepath')
+                    
+                    # プロンプト入力用の行
+                    with gr.Row():
+                        prompt_analysis = PromptAnalysis(tagger_dir)
+                        [prompt, nega] = prompt_analysis.layout(self.input_image_path)           
+                    # 画像の詳細設定用のスライダー行
+                    with gr.Row():
+                        controlnet_conditioning_scale = gr.Slider(minimum=0.5, maximum=1.25, value=1.0, step=0.01, interactive=True, label="線画忠実度")
+                
+                    # 画像生成ボタンの行
+                    with gr.Row():
+                        generate_button = gr.Button("生成", interactive=False)
+
+                with gr.Column():
+                    output_image = gr.Image(type="pil", label="Output Image")
+
+                # インプットとアウトプットの設定
+                inputs = [
+                    input_image_path,
+                    prompt,
+                    nega,
+                    controlnet_conditioning_scale,
+                ]
+                outputs = [output_image]
+                
+                # ボタンのクリックイベントを設定
+                generate_button.click(
+                    fn=self.predict,
+                    inputs=[self.input_image_path, prompt, nega, controlnet_conditioning_scale],
+                    outputs=[output_image]
+                )
+
+        # デモの設定と起動
+        demo.queue(api_open=True)
+        demo.launch(show_api=True)
