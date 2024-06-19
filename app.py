40bfaac05f4689edf25e7e2ee210e279aac83c16
tori29umai0123 <minami.ibon0123@gmail.com>
2024-06-19 20:52:58 +0900

    app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def load_model(lora_dir, cn_dir):
     pipe = StableDiffusionXLControlNetImg2ImgPipeline.from_pretrained(
         "cagliostrolab/animagine-xl-3.1", controlnet=controlnet, vae=vae, torch_dtype=torch.float16
     )
-    # pipe.load_lora_weights(lora_dir, weight_name="sdxl_BWLine.safetensors")
+    pipe.load_lora_weights(lora_dir, weight_name="sdxl_BWLine.safetensors")
     pipe = pipe.to(device)
     return pipe
 
